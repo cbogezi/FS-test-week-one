de8ba8cd99bb4441c7f5b1cfad446077af906fe2
Carol Bogezi <cbogezi@gmail.com>
2021-02-15 11:41:09 -0500

    Carol_Bogezi_Test_1_done

--- README.md
+++ README.md
@@ -25,20 +25,20 @@ Give yourself a grade for each question. Explain why you gave yourself that grad
 
 ### 1. Palindrome
 
-- Grade:
-- Explanation:
+- Grade: 3
+- Explanation: I copied this code and tweaked it abit and it worked. 
 
 ### 2. Add grid items
 
-- Grade:
-- Explanation:
+- Grade: 1
+- Explanation: I spent the most time on this question and I still cannot get it to work. 
 
 ### 3. Create file name string in CamelCase
 
-- Grade:
-- Explanation:
+- Grade: 1.5
+- Explanation: I copied the code from a previous class exercise and got it to work some but I did not get the result that I need. 
 
 ### 4. Create pattern
 
-- Grade:
-- Explanation:
+- Grade: 1.5
+- Explanation: Again I copied the code and got a pattern of sorts but did not get it to do what it should be doing.
--- challenges.js
+++ challenges.js
@@ -9,6 +9,7 @@ Note:
 - All negative numbers should return false.
 - Single numbers should return true.
 
+
 Test cases:
 console.log(isPalindrome(12321)); // true
 console.log(isPalindrome(-12321)); // false
@@ -17,7 +18,40 @@ console.log(isPalindrome(84146)); // false
 console.log(isPalindrome(12)); // false
 ------------------*/
 
-function isPalindrome(num) {}
+function isPalindrome(num) {
+    // Finding the appropriate factor to extract the fisrt digit
+    let factor = 1;
+    while (num / factor >= 10){
+        factor *= 10;
+    }
+    while (num) {
+        let first = Math.floor(num / factor);
+        let last = num % 10;
+        // If first and last digit not same return false
+        if (first != last){
+            return false;
+        }
+        // Removing the first and last digit from number
+        num = Math.floor ((num % factor) / 10);
+        // Reducing factor by a factor of 2 as 2 digits are dropped
+        factor = factor / 100;
+    }
+    return true;
+};
+console.log(isPalindrome(123241));
+console.log(isPalindrome(12321));
+console.log(isPalindrome(-12321));
+console.log(isPalindrome(1)); // true
+console.log(isPalindrome(84146)); // false
+console.log(isPalindrome(12)); // false
+
+
+// start copied from internet
+//const isPalindrome = (num) => {
+//     // Finding the appropriate factor to extract the fisrt digit
+//     let factor = 1;
+// }
+
 
 /*------------------
 2)
@@ -33,7 +67,15 @@ const x = [[[1, 0, -3], [2, 4], [3]], [[4, 4, 3], [5, -3, -1], [6, 10]], [[7, -5
 console.log(addGridItems(x)); // 50
 ------------------*/
 
-function addGridItems(grid) {}
+// function addGridItems(grid) {
+//     let y = [[1], [2]];
+
+//     let x = [[[1, 0, -3], [2, 4], [3]], [[4, 4, 3], [5, -3, -1], [6, 10]], [[7, -5, -5], [8, 0], [9, 1]]];
+// };
+
+// console.log(addGridItems(y)); // 3
+// console.log(addGridItems(x)); // 50
+
 
 /*------------------
 3)
@@ -56,7 +98,35 @@ console.log(createFileName("Sofia Profile Photo", "image")); // "sofia_profile_p
 console.log(createFileName("Data from Pokemon API", "JSON")); // "data_from_pokemon_api.json"
 ------------------*/
 
-function createFileName() {}
+function createFileName (str) {
+    let letter = ""
+    for(i=0; i<str.length; i++){  //looping thru each char in the sentence
+      if(str[i] === " "){ //if char is a space...
+       letter += "_" + str[i+1].toLowerCase() + str.push(FileExtensionName); //lowercase & concat the next letter & a space & add file extension name
+       i++;                              //iterate again to skip over repeating the next letter twice (e.g. WwentTto)
+      } else if (str[i] !== " "){  //doesn't seem to work without else if
+        letter += str[i];  //if not a space, concat char to string
+      }
+    } return letter
+}
+console.log(createFileName("Sofia Engineering Resume", "PDF")); // "sofia_engineering_resume.pdf"
+console.log(createFileName("Sofia Profile Photo", "image")); // "sofia_profile_photo.png"
+console.log(createFileName("Data from Pokemon API", "JSON")); // "data_from_pokemon_api.json"
+
+//COPIED CODE FROM LOOPING CLASS below:
+// function titleCase (str){
+//     let letter = ""
+//     for(i=0; i<str.length; i++){  //looping thru each char in the sentence
+//       if(str[i] === " "){ //if char is a space...
+//        letter += " " + str[i+1].toUpperCase(); //uppercase & concat the next letter & a space
+//        i++;                              //iterate again to skip over repeating the next letter twice (e.g. WwentTto)
+//       } else if (str[i] !== " "){  //doesn't seem to work without else if
+//         letter += str[i];  //if not a space, concat char to string
+//       }
+//     } return letter
+//   } 
+//   console.log(titleCase("He went to the store."))
+//   console.log(titleCase("It's not pretty but it works"))
 
 /*------------------
 4) Print the following pattern
@@ -69,3 +139,12 @@ function createFileName() {}
 Hints:
 - Use a loop within a loop.
 ------------------*/
+let number = '';
+
+for (let x = 99; x >= 88; x--) {
+  number = number + x;
+  console.log(number);
+}
+
+let rowCount = '';
+for (let i = 0; i < names; i++);
